15b04e00ca22e523259c6197a78ec67173039111
Sam Greenwood <sam@samgreenwood.me>
2015-02-10 01:10:40 +1030

    initial

---
+++ announce.go
@@ -0,0 +1,99 @@
+package main
+
+import "os"
+import "os/exec"
+import "fmt"
+import "io/ioutil"
+import "path"
+import "strconv"
+import "bytes"
+import "net/http"
+import "net/url"
+import "path/filepath"
+import "github.com/robfig/config"
+
+func main() {
+
+	filename := os.Args[0];
+	dirname := path.Dir(filename);
+	abspath, _ := filepath.Abs(dirname);
+
+	configFile := abspath + "/" + "announce.conf";
+
+	c, _ := config.ReadDefault(configFile);
+
+	var site, _ = c.String("announce", "site");
+	var announceUrl, _ = c.String("announce", "url");
+	var tthPath, _ = c.String("announce", "tthsum");
+
+	args := os.Args[1:]
+
+	if len(args) == 8 {
+		fmt.Println("SABnzbd mode");
+
+		var downloadPath string = args[0];
+		var processingStatus string = args[6];
+
+		if processingStatus == "0" {
+			fmt.Println("Processing completed successfully");
+
+			announce(site, announceUrl, downloadPath, tthPath);
+		}
+	}
+}
+
+func announce(site string, announceUrl string, downloadPath string, tthPath string) {
+	var largestFile os.FileInfo = getLargestFile(downloadPath);
+	var filename string = largestFile.Name();
+	var size string = strconv.FormatInt(largestFile.Size(), 10);
+	var tth string = calculateTth(tthPath, downloadPath+"/"+largestFile.Name());
+	var magnet string = fmt.Sprintf("magnet:?xt=urn:tree:tiger:%s&xl=%s&dn=%s", tth, size, filename);
+
+	fmt.Println("Sitename: " + site);
+	fmt.Println("Filename: " + filename);
+	fmt.Println("Size: " + size);
+	fmt.Println("TTH: " + tth);
+	fmt.Println("Magnet: " + magnet);
+
+	apiUrl := announceUrl;
+	resource := "/announce";
+	data := url.Values{"site": {site}, "filename": {filename}, "size": {size}, "tth": {tth}}
+
+	u, _ := url.ParseRequestURI(apiUrl);
+	u.Path = resource;
+	urlStr := fmt.Sprintf("%v", u);
+
+	client := &http.Client{};
+	r, _ := http.NewRequest("POST", urlStr, bytes.NewBufferString(data.Encode()));
+	client.Do(r);
+}
+
+func getLargestFile(path string) os.FileInfo {
+	files, _ := ioutil.ReadDir(path);
+
+	var largestFile = files[0];
+
+	for _, f := range files {
+		if f.Size() > largestFile.Size() {
+			largestFile = f;
+		}
+	}
+
+	fmt.Println("Largest File Found: " + largestFile.Name());
+
+	return largestFile;
+}
+
+func calculateTth(tthPath string, filePath string) string {
+
+	out, err := exec.Command(tthPath, filePath).Output()
+
+	if err != nil {
+		fmt.Println(err);
+		fmt.Println("Error calculating TTH");
+	}
+
+	var outputString string = (string(out[:]));
+
+	return outputString[0:39];
+}
